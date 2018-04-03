508b08515c4db6747f72443efff1108f3e355766
李通 <ghostong@126.com>
2018-04-03 14:52:40 +0800

    修改UI

--- index.php
+++ index.php
@@ -43,20 +43,45 @@ function PageIndex () {
     $ListStr = '';
     foreach ( $FileList as $k => $v ) {
         $date = date('Y-m-d H:i:s', $v['time']);
-        $ListStr .= "<li>[$date] {$v['Name']} <a href='/index.php?action=download&id={$k}'>下载</a> <a href='/index.php?action=delete&id={$k}'>删除</a></li>";
+        $ListStr .= "<li>[$date] {$v['Name']} &nbsp;&nbsp;[&nbsp;<a href='/index.php?action=download&id={$k}'>下载</a>&nbsp;/&nbsp;<a href='/index.php?action=delete&id={$k}'>删除</a>&nbsp;]</li>";
+    }
+    if (!$ListStr && !is_dir(FILE_DIR)) {
+        $ListStr = '<li><a href="/index.php?action=init">初始化存储目录</a></li>';
     }
     echo <<<POUT
 <html>
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="blank">
+        <meta name="format-detection" content="telephone=no">
+        <style>
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                height: 100%;
+                color: #646464;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+            a:link , a:visited , a:hover , a:active {color: #111111}
+            a{text-decoration:none}
+            .spadding {
+                padding:20px;
+            }
+        </style>
     </head>
     <body>
-        <div>
+        <div class="spadding">
             <ul>
                 $ListStr
             </ul>
         </div>
-        <div>
+        <hr/>
+        <div class="spadding">
             <form enctype="multipart/form-data" method="POST" action="/index.php?action=upload">
                 <input type="file" name="uploadfile" />
                 <input type="submit" value="上传" />
@@ -73,6 +98,10 @@ function PageTips ( $Url='/', $Time=2, $Tips = '操作成功' ) {
 <html>
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="blank">
+        <meta name="format-detection" content="telephone=no">
         <meta http-equiv="Refresh" content="{$Time}; url={$Url}" />
         <style>
             body {
