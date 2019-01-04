48190f31a830ab13df0916affed21cccf5a40454
李通 <ghostong@126.com>
2019-01-04 17:57:23 +0800

    修改布局,添加字符串

--- index.php
+++ index.php
@@ -25,7 +25,11 @@ switch ( $Action ) {
         DownLoad ($_GET['id']) ;
         break;
     case 'upload' :
-        MvFile ($_FILES["uploadfile"]);
+        if ( $_FILES["uploadfile"] ) {
+            MvFile ($_FILES["uploadfile"]);
+        }else{
+            InsertStr();
+        }
         break;
     case 'delete' :
         Delete($_GET['id']);
@@ -47,14 +51,23 @@ function PageIndex () {
     $LogoUrl =  QRUrl(SelfUrl());
     $FileList = EasyKvReadAll();
     $ListStr = '';
+    $ListStr2 = '';
     foreach ( $FileList as $k => $v ) {
-        $Url = "/index.php?action=download&id={$k}";
-        $UrlQr = QRUrl(SelfUrl().$Url);
-        $date = date('Y-m-d H:i:s', $v['time']);
-        $ListStr .= "<li>[$date] {$v['Name']} &nbsp;&nbsp;[&nbsp;<a href='{$UrlQr}'>二维码</a>&nbsp;/&nbsp;<a href='{$Url}'>下载</a>&nbsp;/&nbsp;<a href='/index.php?action=delete&id={$k}'>删除</a>&nbsp;]</li>";
+        if($v['type'] == 'f') {
+            $Url = "/index.php?action=download&id={$k}";
+            $UrlQr = QRUrl(SelfUrl().$Url);
+            $date = date('m-d H:i', $v['time']);
+            $ShortName = mb_substr($v['Name'],0,20,'utf8');
+            $ListStr .= "<tr><td>[$date]</td><td><a href='{$Url}' title='点击下载({$v['Name']})'>{$ShortName}</a></td><td>&nbsp;&nbsp;&nbsp;&nbsp;[&nbsp;<a href='{$UrlQr}' target='_blank' title='通过二维码下载({$v['Name']})'>二维码</a>&nbsp;/&nbsp;<a href='/index.php?action=delete&id={$k}' title='删除({$v['Name']})'>删除</a>&nbsp;]<td></tr>";
+        }elseif($v['type'] == 's'){
+            $OutStr = htmlentities ( $v['str'] );
+            #$OutStr = str_replace (array("\r\n","\n\r","\n","\r"),"<br/>",$OutStr);
+            #$OutStr = str_replace (array(" "),"&nbsp;",$OutStr);
+            $ListStr2 .= "<tr><td><textarea style='width:500px;height:300px;'>{$OutStr}</textarea><td></tr>";
+        }
     }
     if (!$ListStr && !is_dir(FILE_DIR)) {
-        $ListStr = '<li><a href="/index.php?action=init">初始化存储目录</a></li>';
+        $ListStr = '<tr><td style="padding:30px;"><a href="/index.php?action=init" style="color:#ff0000;">初始化存储目录</a></td></tr>';
     }
     echo <<<POUT
 <html>
@@ -86,27 +99,49 @@ function PageIndex () {
         </style>
     </head>
     <body>
-        <div class="spadding" style="height:100px">
-            <div class="floatleft">
-                <img src="{$LogoUrl}" style="height:80px;"/>
-            </div>
-            <div class="floatleft" style="font-size:60px;">
-                &nbsp; EasyShare
-            </div>
-        </div>
+        <table class="spadding">
+            <tr>
+                <td>
+                    <img src="{$LogoUrl}" style="height:80px;"/>
+                </td>
+                <td style="font-size:60px;">
+                    EasyShare
+                </td>
+            </tr>
+        </table>
         <hr/>
-        <div class="spadding">
-            <ul>
-                $ListStr
-            </ul>
-        </div>
+        <table class="spadding" style="width:100%">
+            <tr valign="top">
+                <td style="border-right: 1px solid; width: 50%">
+                    <table>
+                        $ListStr
+                    </table>
+                </td>
+                <td style="border-left: 1px solid" class="spadding">
+                    <table>
+                        $ListStr2
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td style="border-right: 1px solid">
+                    <div class="spadding">
+                        <form enctype="multipart/form-data" method="POST" action="/index.php?action=upload">
+                            <input type="file" name="uploadfile" />
+                            <input type="submit" value="上传" />
+                        </form>
+                    </div>
+                </td>
+                <td style="border-left: 1px solid" class="spadding">
+                    <div class="spadding">
+                        <form enctype="multipart/form-data" method="POST" action="/index.php?action=upload">
+                            <textarea name="str" style="float:left;height:50px;width:300px;"></textarea> <input type="submit" value="提交" style="margin-left:50px;margin-top:10px;"/>
+                        </form>
+                    </div>
+                </td>
+            </tr>
+        </table>
         <hr/>
-        <div class="spadding">
-            <form enctype="multipart/form-data" method="POST" action="/index.php?action=upload">
-                <input type="file" name="uploadfile" />
-                <input type="submit" value="上传" />
-            </form>
-        </div>
     </body>
 </html>
 POUT;
@@ -140,11 +175,13 @@ function PageTips ( $Url='/', $Time=2, $Tips = '操作成功' ) {
                 vertical-align: middle;
                 font-size: 54px;
             }
+            a:link , a:visited , a:hover , a:active {color: #B0BEC5}
+            a{text-decoration:none}
         </style>
     </head>
     <body>
         <div class="container">
-            {$Tips}
+            <a href="/">{$Tips}</a>
         </div>
     </body>
 </html>
@@ -172,7 +209,7 @@ function DownLoad ($id) {
 
 function Delete ( $id ) {
     $Data = EasyKvGet($id) ;
-    EasyKvDB($id,'','del');
+    EasyKvDel($id);
     unlink(FILE_DIR.'/'.$Data['SaveName']);
     PageTips('/',2,$Data['Name'].'删除成功');   
 }
@@ -203,7 +240,7 @@ function MvFile ( $FileInfo ) {
         $FileId = md5($FileInfo['name']);
         $FileName = $FileId.'.'.$PathInfo['extension'];
         if ( move_uploaded_file ( $FileInfo['tmp_name'] ,FILE_DIR.'/'.$FileName) ) {
-            EasyKvDB($FileId,array('Name'=>$FileInfo['name'],'Size'=>$FileInfo['size'],'SaveName'=>$FileName,'time'=>time()));
+            EasyKvSet($FileId,array('Name'=>$FileInfo['name'],'Size'=>$FileInfo['size'],'SaveName'=>$FileName,'time'=>time(),'type'=>'f'));
             PageTips('/',2,'文件上传成功');
         } else {
             PageTips('/',2,'文件上传失败');
@@ -214,30 +251,54 @@ function MvFile ( $FileInfo ) {
 
 }
 
+function InsertStr () {
+    $Str = $_POST['str'] ;
+    if ( $Str ) {
+        $Key = md5('TextArea');
+        EasyKvSet($Key,array('str'=>$Str,'time'=>time(),'type'=>'s'));
+        PageTips('/',2,'提交成功');
+    }else{
+        PageTips('/',2,'提交失败');
+    }
+}
+
 function SelfUrl () {
     return ( strtolower( current( explode('/',$_SERVER['SERVER_PROTOCOL']) ) ).'://'.$_SERVER['HTTP_HOST'] );
 }
 
-function EasyKvDB ( $k, $v , $Action = '' ) {
-    $Str = file_get_contents( DB_FILE );
-    $Arr = unserialize($Str);
-    if ($Action == 'del') {
-        unset($Arr[$k]);
-    }else{
-        $Arr[$k] = $v;
-    }
-    $Str = serialize($Arr);
-    return file_put_contents ( DB_FILE , $Str );
+
+
+//暂不考虑并发情况
+
+function EasyKvSet ( $k, $v ) {
+    $Arr=EasyKvOpen();
+    $Arr[$k] = $v;
+    return EasyKvWrite( $Arr );
 }
 
 function EasyKvReadAll () {
+    $Arr=EasyKvOpen();
+    return $Arr;
+}
+
+function EasyKvGet ( $k ) {
+    $Arr=EasyKvOpen();
+    return $Arr[$k] ? $Arr[$k] : false;
+}
+
+function EasyKvDel ( $k ) {
+    $Arr=EasyKvOpen();
+    unset($Arr[$k]);
+    return EasyKvWrite( $Arr );
+}
+
+function EasyKvOpen () {
     $Str = file_get_contents( DB_FILE );
     $Arr = unserialize($Str);
     return $Arr;
 }
 
-function EasyKvGet ( $k ) {
-    $Str = file_get_contents( DB_FILE );
-    $Arr = unserialize($Str);
-    return $Arr[$k] ? $Arr[$k] : false;
+function EasyKvWrite ( $Arr ) {
+    $Str = serialize($Arr);
+    return file_put_contents ( DB_FILE , $Str );
 }
